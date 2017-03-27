7073f51ce67a48406f39dc41d6198a8f297c494f
olivier <colombe.oliv@gmail.com>
2017-03-27 16:00:10 -0400

    commit de 16:00

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -1,137 +1,107 @@
 <?php
 namespace Controller;
 
-use \W\Controller\Controller;
 use \Model\UsersModel;
+use \W\Controller\Controller;
 use \W\Security\AuthentificationModel;
-
-
-
 
 class UserController extends Controller
 {
 
-	public function authUser1()
-	{
-		$error = array(); 
-		$post = array();
-		$success = false;
+    public function authUser1()
+    {
+        $error   = array();
+        $post    = array();
+        $success = false;
 
-		if(!empty($_POST))
-		{
-			foreach($_POST as $key => $value)
-			{
-				$post[$key] = trim(strip_tags($value));
-			}
+        if (!empty($_POST)) {
+            foreach ($_POST as $key => $value) {
+                $post[$key] = trim(strip_tags($value));
+            }
 
-			if(strlen($post['username']) < 3)
-			{
-				$error[] = 'Le prénom doit faire au moins 3 caractères';
-			}
+            if (strlen($post['username']) < 3) {
+                $error[] = 'Le prénom doit faire au moins 3 caractères';
+            }
 
-			if(strlen($post['lastname']) < 3)
-			{
-				$error[] = 'Le nom doit faire au moins 3 caractères';
-			}
+            if (strlen($post['lastname']) < 3) {
+                $error[] = 'Le nom doit faire au moins 3 caractères';
+            }
 
-			if(!filter_var($post['email'], FILTER_VALIDATE_EMAIL))
-			{
-				$error[] = 'L\'adresse email est invalide';
-			}
+            if (!filter_var($post['email'], FILTER_VALIDATE_EMAIL)) {
+                $error[] = 'L\'adresse email est invalide';
+            }
 
-			if(strlen($post['password']) < 3|| strlen($post['password']) > 20)
-			{
-				$error[] = 'Le mot de passe doit comporter entre 8 et 20 caractères maximum';
-			}  else {
-				$passwordHash = password_hash($post["password"], PASSWORD_DEFAULT);
-			}
+            if (strlen($post['password']) < 3 || strlen($post['password']) > 20) {
+                $error[] = 'Le mot de passe doit comporter entre 8 et 20 caractères maximum';
+            } else {
+                $passwordHash = password_hash($post["password"], PASSWORD_DEFAULT);
+            }
 
-			if(count($error) > 0)
-			{		
-				$formError = true;
-			}
-			else {
+            if (count($error) > 0) {
+                $formError = true;
+            } else {
 
-				$datas = [
-					// colonne sql => valeur à insérer
-				'username' => $post['username'],
-				'lastname' => $post['lastname'],
-				'email'		 => $post['email'],
-				'password' => $passwordHash
-				];
-				$User = new UsersModel();
-				$User->insert($datas);
+                $datas = [
+                    // colonne sql => valeur à insérer
+                    'username' => ucfirst($post['username']),
+                    'lastname' => ucfirst($post['lastname']),
+                    'email'    => $post['email'],
+                    'password' => $passwordHash,
+                ];
+                $User = new UsersModel();
+                $User->insert($datas);
+            }
+        }
+        $params = [
+            'success' => $success,
+            'error'   => $error,
+        ];
 
-			}
+        $this->show('users/authUser', $params);
+    }
 
-		}
-		$params = [
+    public function loginUser()
+    {
+        //traiter le formulaire contact ici...
+        $err     = [];
+        $success = false;
+        $post    = [];
 
-		'success' => $success,
-		'error'  => $error,
-		];
-		$this->show('users/authUser',$params);
-	}
+        if (!empty($_POST)) {
 
+            foreach ($_POST as $key => $value) {
+                $post[$key] = trim(strip_tags($value));
+            }
 
+            if (!filter_var($post['ident'], FILTER_VALIDATE_EMAIL)) {
+                $err[] = 'Veuillez saisir votre identifiant';
+            }
 
+            if (empty($post['passwd']) || strlen($post['passwd']) < 3) {
 
-	public function loginUser()
-	{
-		//traiter le formulaire contact ici...
-		$err     = [];
-		$success = false;
-		$post = [];
-		
+                $err[] = 'Veuillez saisir votre mot de passe';
+            }
 
-		if (!empty($_POST)) {
+            if (count($err) > 0) {
+                echo implode('<br>', $err);
+            } else {
+                $User = new AuthentificationModel();
+                $id   = $User->isValidLoginInfo($post['ident'], $post['passwd']);
+                if ($id) {
+                    $ident   = new UsersModel();
+                    $tmpUser = $ident->find($id);
+                    $User->logUserIn($tmpUser);
+                    $this->redirectToRoute('article_ajaxArticles');
+                } else {
+                    $err[] = "Vous n'etes pas identifié";
+                }
+            }
+        }
+        $params = [
+            'success' => $success,
+            'err'     => $err,
 
-			foreach ($_POST as $key => $value) {
-				$post[$key] = trim(strip_tags($value));
-			}
-
-			if (!filter_var($post['ident'], FILTER_VALIDATE_EMAIL)) {
-				$err[] = 'Veuillez saisir votre identifiant';
-			}
-
-			if (empty($post['passwd'])) {
-
-				$err[] = 'Veuillez saisir votre mot de passe';
-			}
-
-			if (count($err) === 0) {
-				
-				$User = new AuthentificationModel();
-				if($User->isValidLoginInfo($post['ident'], $post['passwd'])){ 
-					$success = true;
-					
-
-				} else {
-					echo"vous n'etes pas connecte";
-				}
-			} 
-		}
-		$params = [
-
-		'success' => $success,
-		'err'  => $err,
-		];
-		$this->show('users/loginUser', $params);
-
-	}
-
-
-	
-	public function Useredirect() {
-		if (!empty($_POST)) {
-	//redirige vers une autre page interne
-			$this->redirectToRoute('article_ajaxArticles');
-		}
-	}
-
-
-
-
-	
-
+        ];
+        $this->show('users/loginUser', $params);
+    }
 }
--- app/routes.php
+++ app/routes.php
@@ -3,9 +3,8 @@
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'default_home'],
 		['GET|POST', '/article/ajaxArticles', 'Article#ajaxArticles', 'article_ajaxArticles'],
-		['GET|POST', '/article/listArticles', 'Article#listArticles', 'article_listArticles'],
-		['GET|POST', '/article/viewArticle/[i:id]', 'Article#ViewArticle', 'article_viewArticle'],
+		['GET', '/article/listArticles', 'Article#listArticles', 'article_listArticles'],
+		['GET', '/votre_article/[i:id]', 'Article#ViewArticle', 'article_viewArticle'],
 		['GET|POST', '/users/loginUser', 'User#loginUser', 'user_loginUser'],
-		['GET|POST', '/users/authUser', 'User#authUser1', 'user_authUser'],
-		['GET|POST', '/users/loginUser', 'User#Useredirect', 'user_Useredirect'],
+		['GET|POST', '/users/connexion', 'User#authUser1', 'user_authUser'],		
 	);
