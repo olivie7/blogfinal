8e50fa6e3eb5651f7ceb1240534d22668918ee58
olivie7 <colombe.oliv@gmail.com>
2017-03-28 17:27:02 -0400

    ok commit de chez moi

--- app/routes.php
+++ app/routes.php
@@ -6,5 +6,5 @@
 		['GET', '/article/listArticles', 'Article#listArticles', 'article_listArticles'],
 		['GET', '/votre_article/[i:id]', 'Article#ViewArticle', 'article_viewArticle'],
 		['GET|POST', '/users/loginUser', 'User#loginUser', 'user_loginUser'],
-		['GET|POST', '/users/connexion', 'User#authUser1', 'user_authUser'],		
+		['GET|POST', '/users/connexion', 'User#authUser1', 'ok'],		
 	);
